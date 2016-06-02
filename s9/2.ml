b51765a482b2c0ffc9f3f4150d5e69121aa77931
Tomy <kazushige.tominaga@mkt-apps.com>
2016-06-02 18:38:47 +0900

    9-1, 9-2

---
+++ s9/2.ml
@@ -0,0 +1,6 @@
+#use "s8/3.ml";;
+
+{name = "ほげ"; height = 160.0; weight = 50.0; birthday = "0119"; blood_type = "A";} ::
+{name = "ふが"; height = 165.0; weight = 55.0; birthday = "0111"; blood_type = "B";} ::
+{name = "ほげふが"; height = 170.0; weight = 60.0; birthday = "0222"; blood_type = "O";} ::
+[]
