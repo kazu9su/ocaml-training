f41d8f8a947770bdb2555221cd3cbe3c0cb0f1bb
Tomy <kazushige.tominaga@mkt-apps.com>
2016-06-02 18:56:02 +0900

    9-3

--- s9/2.ml
+++ s9/2.ml
@@ -1,6 +1,7 @@
 #use "s8/3.ml";;
 
-{name = "ほげ"; height = 160.0; weight = 50.0; birthday = "0119"; blood_type = "A";} ::
-{name = "ふが"; height = 165.0; weight = 55.0; birthday = "0111"; blood_type = "B";} ::
-{name = "ほげふが"; height = 170.0; weight = 60.0; birthday = "0222"; blood_type = "O";} ::
-[]
+let person_a = {name = "ほげ"; height = 160.0; weight = 50.0; birthday = "0119"; blood_type = "A";}
+let person_b = {name = "ふが"; height = 165.0; weight = 55.0; birthday = "0111"; blood_type = "B";}
+let person_c = {name = "ほげふが"; height = 170.0; weight = 60.0; birthday = "0222"; blood_type = "O";};;
+
+person_a :: person_b :: person_c :: []
---
+++ s9/3.ml
@@ -0,0 +1,4 @@
+#use "s9/2.ml";;
+
+["春"; "夏"; "秋"; "冬";];;
+[person_a; person_b; person_c]
