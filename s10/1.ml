b84c3bca6355a8f9d2c1e932582b04a77cc529c0
Tomy <kazushige.tominaga@mkt-apps.com>
2016-07-31 12:31:25 +0900

    update 10-1

--- s10/1.ml
+++ s10/1.ml
@@ -1,15 +1,3 @@
-(* 2つのリストを渡すとマージしたリストを返す *)
-(* merge_list : int list list -> int list list -> int list list *)
-let rec merge_list lst1 lst2 = match lst2 with
-    [] -> lst1
-    | first :: rest -> merge_list (first :: lst1) rest
-
-let test1 = merge_list [3; 4; 5; 6] [2; 1] = [1; 2; 3; 4; 5; 6;]
-let test2 = merge_list [1] [] = [1]
-let test3 = merge_list [] [] = []
-let test4 = merge_list [] [1] = [1]
-
-
 (* 目的:予め昇順に並んでいる整数のリストlstを受け取ったら、lstを前から順番に見ていき、昇順となる位置にnを挿入したリストを返す *)
 (* insert : int list lst -> int -> int list lst *)
 let rec insert lst n = match lst with
