8cc416957fa0387e5979ca9a3c3b1bcc7e384050
Tomy <kazushige.tominaga@mkt-apps.com>
2016-09-08 19:12:18 +0900

    add 5.ml

---
+++ s10/5.ml
@@ -0,0 +1,23 @@
+type gakusei_t = {
+    namae: string;
+    tensuu: int;
+    seiseki: string;
+}
+
+(* 目的: gakusei_t型のリストを受け取ったらlstの中で最高得点を取った人のレコードを返す *)
+let rec gakusei_max lst = match lst with
+    [] -> {namae = ""; tensuu = min_int; seiseki = ""}
+    | ({namae = n; tensuu = t; seiseki = s;} as first) :: rest ->
+        match (gakusei_max rest) with
+            ({namae = r_n; tensuu = r_t; seiseki = r_s;} as result) ->
+                if t >= r_t
+                then first
+                else gakusei_max rest
+
+let test1 = gakusei_max [] = {namae = ""; tensuu = min_int; seiseki = ""}
+let test2 = gakusei_max [
+    {namae = "hoge"; tensuu = 10; seiseki = "A"};
+    {namae = "fuga"; tensuu = 50; seiseki = "A"};
+    {namae = "hogehoge"; tensuu = 30; seiseki = "A"};
+
+] = {namae = "fuga"; tensuu = 50; seiseki = "A"}
