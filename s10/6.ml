6bfc396915cf1493a6eccc55697abdc2b558a02d
Tomy <kazushige.tominaga@mkt-apps.com>
2016-09-15 18:17:30 +0900

    adjust

--- s10/6.ml
+++ s10/6.ml
@@ -10,8 +10,8 @@ let rec gakusei_max lst = match lst with
     [] -> {namae = ""; tensuu = min_int; seiseki = ""}
     | ({namae = n; tensuu = t; seiseki = s;} as first) :: rest ->
         let max_rest = gakusei_max rest in 
-        match (max_rest) with
-            ({namae = r_n; tensuu = r_t; seiseki = r_s;} as result) ->
+        match max_rest with
+            {namae = r_n; tensuu = r_t; seiseki = r_s;} ->
                 if t >= r_t
                 then first
                 else max_rest
