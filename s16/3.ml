5b3abf79250b69f01eab6622d3500464394ec859
Tomy <kazushige.tominaga@mkt-apps.com>
2017-04-21 10:06:38 +0900

    fix specification

--- s16/3.ml
+++ s16/3.ml
@@ -4,7 +4,7 @@
 
 (* 目的: 直前に確定した駅p(eki_t型)と未確定の駅のリストv(eki_t list型)を受け取ったら、
  * 必要な更新処理を行った後の未確定の駅のリストを返す *)
-(* koushin : eki_t -> eki_t list -> eki_t list *)
+(* koushin : eki_t -> eki_t list -> ekikan_t list -> eki_t list *)
 let koushin = fun p v lst -> match p with
     {namae = n_p; saitan_kyori = s_p; temae_list = t_p} ->
     List.map( fun q -> match q with
