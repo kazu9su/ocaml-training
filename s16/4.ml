6c80199d5790e9c08e732cf79451d3d5d2b85bf7
Tomy <kazushige.tominaga@mkt-apps.com>
2017-05-11 19:59:18 +0900

    add

--- s16/4.ml
+++ s16/4.ml
@@ -1,24 +1,34 @@
 #use "metro.ml";;
 #use "s15/4.ml";;
-#use "s16/3.ml";;
+#use "s10/11.ml";;
+
+let koushin = fun p v lst -> match p with
+    {namae = n_p; saitan_kyori = s_p; temae_list = t_p} ->
+    List.map( fun q -> match q with
+        {namae = n_q; saitan_kyori = s_q; temae_list = t_q} ->
+            let kyori = get_ekikan_kyori n_p n_q lst in
+            if ((s_p +. kyori) < s_q) then
+                {namae = n_q; saitan_kyori = s_p +. kyori; temae_list = n_p::t_q}
+            else
+                q
+    ) v
 
 (* 目的: eki_t 型の未確定の駅のリストとekikan_t list 型の駅間のリストを
  * 受け取ったら、ダイクストラのアルゴリズムにしたがって
  * 各駅について最短距離と最短経路が正しく入ったリスト(eki_t list 型) を返す*)
-let dijkstra_main eki_t_list ekikan_t_list =
-    let rec dij eki_t_list ekikan_t_list result = match eki_t_list with
-    [] -> result
-    | first::rest ->
-        let tpl = saitan_wo_bunri eki_t_list
-        in match tpl with
-            (saitan, rest) -> let res = koushin saitan rest ekikan_t_list
-            in dij rest ekikan_t_list res
-    in dijkstra_main eki_t_list ekikan_t_list []
+let rec dijkstra_main eki_t_list ekikan_t_list = match eki_t_list with
+    [] -> []
+    | lst ->
+        let (saitan, nokori) = saitan_wo_bunri lst in
+        let eki_t_list2 = koushin saitan nokori ekikan_t_list in
+        saitan :: dijkstra_main eki_t_list2 ekikan_t_list
 
-(*
- let test1 = dijkstra_main [
-    {namae = "代々木上原"; saitan_kyori = 0.; temae_list = [];};
+let test1 = dijkstra_main [
+    {namae = "代々木上原"; saitan_kyori = 0.; temae_list = ["代々木上原"];};
     {namae = "代々木公園"; saitan_kyori = infinity; temae_list = []};
     {namae = "明治神宮前"; saitan_kyori = infinity; temae_list = []};
-] global_ekikan_list
-*)
+] global_ekikan_list = [
+    {namae = "代々木上原"; saitan_kyori = 0.; temae_list = ["代々木上原"]};
+    {namae = "代々木公園"; saitan_kyori = 1.; temae_list = ["代々木上原"]};
+    {namae = "明治神宮前"; saitan_kyori = 2.2; temae_list = ["代々木公園"]}
+]
